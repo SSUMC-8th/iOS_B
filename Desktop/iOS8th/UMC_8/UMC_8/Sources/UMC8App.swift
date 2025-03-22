a703632b06a901608f91dd8d2171a9ae569a8a09
Sominjun <somongi@naver.com>
2025-03-22 23:50:54 +0900

    Fix: 로그인 뷰 수정 및 프로젝트 정리

--- Desktop/iOS8th/UMC_8/UMC_8/Sources/UMC8App.swift
+++ Desktop/iOS8th/UMC_8/UMC_8/Sources/UMC8App.swift
@@ -4,7 +4,8 @@ import SwiftUI
 struct UMC8App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
+            
         }
     }
 }
--- Desktop/iOS8th/UMC_8/UMC_8/Sources/VIews/LoginView.swift
+++ Desktop/iOS8th/UMC_8/UMC_8/Sources/VIews/LoginView.swift
@@ -8,66 +8,93 @@
 
 
 
+
+import Foundation
 import SwiftUI
 
-struct InputLoginView: View {
-    var body: some View {
-        VStack{
-            Text("아이디")
-                .frame(width: 401, height: 20, alignment: .leading)
-                .font(.PretendardRegular13)
-                .shadow(color: .gray, radius: 10)
-            Divider()
-            
-            Spacer().frame(height: 47)
-            
-            Text("비밀번호")
-                .frame(width: 401, height: 20, alignment: .leading)
-                .font(.PretendardRegular13)
-                .shadow(color: .gray, radius: 10)
-            Divider()
-            
-            Spacer().frame(height: 47)
-            
-            Button(action: {
-                        print("Hello")
-            }, label: {
-                Text("로그인하기")
-                    .foregroundStyle(Color.white)
-                    .frame(maxWidth: 402, maxHeight: 46)
-            })
-            .frame(width: 402, height: 46)
-            .buttonStyle(.borderedProminent) // 버튼 스타일 적용
-            .tint(Color("#01A862")) // 버튼 색상 적용
-            .font(.PretendardMedium16)
-            .cornerRadius(20)
-        }
-    }
-}
-
-struct TitleView: View {
-    var body: some View {
-        VStack(alignment: .leading){
-            Image("starbucks_Logo")
-                .resizable()
-                .frame(width: 97, height: 95)
-                .shadow(color: .gray, radius: 10,x:5, y: 5)
-            
-            Spacer().frame(height: 28)
-            
-            Text("안녕하세요.\n스타벅스입니다.")
-                .font(.PretendardBold24)
+struct LoginView: View {
+    @State private var id: String = ""
+    @State private var password: String = ""
     
-            
-            Text("회원 서비스 이용을 위해 로그인 해주세요")
-                .frame(width: 402, height: 19,alignment: .leading)
-                .font(.PretendardMedium16)
-                .foregroundStyle(Color("#BFBDBD"))
-                .shadow(color: .gray, radius: 10,x:5, y: 5)
-            
-            
+    var body: some View {
+        VStack {
+            Spacer().frame(height: 104) // 상단 여백
+            loginLogo
+            loginTextField
+            snsLogin
+            Spacer().frame(height: 90.95) // 하단 여백
+                
         }
     }
+    // 로그인 상단뷰
+    private var loginLogo: some View {
+        Group {
+            VStack(alignment: .leading) {
+                Image("Starbuckslogo")
+                    .resizable()
+                    .frame(width: 97, height: 95)
+                Spacer().frame(height: 29)
+                Text("안녕하세요.\n스타벅스입니다!")
+                    .font(.PretendardExtraBold24)
+                    .padding(.bottom, 15)
+                Text("회원 서비스 이용을 위해 로그인 해주세요")
+                    .font(.PretendardMedium16)
+                    .foregroundStyle(Color(.gray))
+            }
+            .frame(maxWidth: 402, minHeight: 270, alignment: .leading)
+            .padding(19)
+        }
+    }
+    // 로그인 텍스트필드
+    private var loginTextField: some View {
+        Group {
+            VStack {
+                TextField("아이디", text: $id)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .font(.PretendardRegular13)
+                Divider()
+                Spacer().frame(height: 20)
+                SecureField("비밀번호", text: $password)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .font(.PretendardRegular13)
+                Divider()
+                Spacer().frame(height: 47)
+                Button(action: {
+                    print("로그인 버튼")
+                }) {
+                    Text("로그인하기")
+                        .font(.PretendardMedium16)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, maxHeight: 46)
+                        .background(Color("green01"))
+                        .cornerRadius(20)
+                }            }
+        }
+        .frame(maxWidth: 402, minHeight: 180)
+        .padding(19)
+    }
+    // 하단 sns 로그인
+    private var snsLogin: some View {
+        VStack(alignment: .center) {
+            Text("이메일로 회원가입하기")
+                .font(.PretendardRegular12)
+                .foregroundStyle(Color(.gray))
+                .underline()
+                .padding(.bottom, 10)
+            Image("kakaologinbutton")
+                .resizable()
+                .frame(width: 305, height: 45)
+                .padding(.bottom, 10)
+            Image("appleloginbutton")
+                .resizable()
+                .frame(width: 305, height: 45)
+        }
+        .frame(maxWidth: 306, minHeight: 104)
+        .padding(.top, 30)
+    }
+    
 }
 
-
+#Preview {
+    LoginView()
+}
